216d3f7e493b3c078022822095e3032195cf7e71
Mithu-12 <mithuvowmick96@gmail.com>
2022-01-01 03:24:13 +0800

    first

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="text-center">
+            <p>Terms of use  |  Privacy policy </p>
+        </div>
+    );
+};
+
+export default Footer;
